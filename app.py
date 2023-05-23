062d75c1b4408ebedf6f0320c0d3df005511ae0f
RianaKartika <rianakartika03@gmail.com>
2023-05-23 13:21:24 +0700

    second commit

--- app.py
+++ app.py
@@ -1,8 +1,23 @@
+import os
+
+from os.path import join, dirname
+
+from dotenv import load_dotenv
+
 from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
 
-client = MongoClient('mongodb+srv://Riana:sandinya@cluster0.qnbeqfw.mongodb.net/?retryWrites=true&w=majority')
-db = client.dbsparta
+dotenv_path = join(dirname(__file__), "env")
+
+load_dotenv(dotenv_path)
+
+MONGODB_URI = os.environ.get("mongodb+srv://Riana:sandinya@cluster0.qnbeqfw.mongodb.net/?retryWrites=true&w=majority")
+
+DB_NAME = os.environ.get("dbsparta")
+
+client = MongoClient(MONGODB_URI) 
+
+db = client[DB_NAME]
 
 app = Flask(__name__)
 
