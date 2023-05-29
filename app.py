54ea73e419ef8b2ba4244c82faf146aa68685688
RianaKartika <rianakartika03@gmail.com>
2023-05-29 09:43:50 +0700

    app.py

--- app.py
+++ app.py
@@ -11,9 +11,9 @@ dotenv_path = join(dirname(__file__), "env")
 
 load_dotenv(dotenv_path)
 
-MONGODB_URI = os.environ.get("mongodb+srv://Riana:sandinya@cluster0.qnbeqfw.mongodb.net/?retryWrites=true&w=majority")
+MONGODB_URI = os.environ.get("MONGODB_URI")
 
-DB_NAME = os.environ.get("dbsparta")
+DB_NAME = os.environ.get("DB_NAME")
 
 client = MongoClient(MONGODB_URI) 
 
